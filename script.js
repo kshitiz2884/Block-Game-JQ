6642fa9b2268bd13834e92faea307d13f8147911
kshitiz2884 <kshitiz.sri@gmail.com>
2023-08-16 15:51:11 +0530

    Code with comments

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
+  // counting number of steps
   let count = 0;
   $("#count").text(++count);
+  // Mapping id and related neighbours with their Ids as an object array
   $("td").on("click", function () {
     var idMappings = {
       1: ["1", "2", "4"],
@@ -13,26 +15,29 @@ $(document).ready(function () {
       8: ["5", "7", "8", "9"],
       9: ["6", "8", "9"],
     };
+    // Method for checking and toggling the element and their neighbours
     var idsToToggle = idMappings[this.id];
     idsToToggle.forEach(function (id) {
-        var $element = $("#" + id);
-        var currentColor = $element.css("background-color");
-        var newColor =
+      var $element = $("#" + id);
+      var currentColor = $element.css("background-color");
+      var newColor =
         currentColor === "rgb(0, 0, 0)" ? "rgba(0, 0, 0, 0)" : "rgb(0, 0, 0)";
-        $element.css("background-color", newColor);
+      $element.css("background-color", newColor);
     });
-    
+    // Incrementing and displaying count
     $("#count").text(++count);
+    // Checking the final codition, all block colours should be same
     var allColorsSame = true;
     var firstColor = $("#" + idsToToggle[0]).css("background-color");
     for (let index = 1; index <= 9; index++) {
-          console.log($("#" + index).css("background-color"));
-          if ($("#" + index).css("background-color") !== firstColor) {
-          allColorsSame = false;
-        }
+      console.log($("#" + index).css("background-color"));
+      if ($("#" + index).css("background-color") !== firstColor) {
+        allColorsSame = false;
+      }
     }
+    //  Displaying Allert if game completed
     if (allColorsSame) {
-      alert("You Won in "+count+" moves");
+      alert("You Won in " + count + " moves");
       location.reload();
     }
   });
